04b6641110ee6971ea33988d7b31739257c92590
Stanislav <stazilastas@gmail.com>
2023-09-14 09:49:45 -0500

    CafeCollection 1.0

--- CafeCollectionStajila/ViewController.swift
+++ CafeCollectionStajila/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var items: [String] = ["Cappuccino", "Latte", "Frappuccino", "Coffee", "Tea"]
+    var prices: [Double] = [5.5, 4.5, 5.0, 3.5, 3.0]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for i in 0 ..< items.count{
+            print("\(items[i]): $\(prices[i])")
+        }
+        
     }
 
 
+   
 }
 
