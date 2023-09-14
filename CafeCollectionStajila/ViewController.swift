d56113316d5335543b341cbe4876d9988233d399
Stanislav <stazilastas@gmail.com>
2023-09-14 09:39:12 -0500

    Initial Commit

---
+++ CafeCollectionStajila/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CafeCollectionStajila
+//
+//  Created by STANISLAV STAJILA on 9/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
